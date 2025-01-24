875391870b5f37549c1c4f23fd89f775a7eec759
shadielfares <shadi.elfares@gmail.com>
2025-01-23 19:55:28 -0500

    Fixes issue #2 copying file to 2D Character Array

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathFinder.java
@@ -0,0 +1,11 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import java.util.ArrayList;
+
+public class PathFinder{
+    private static ArrayList<ArrayList<Character>> mazeArray = new ArrayList<ArrayList<Character>>();
+
+    public ArrayList<ArrayList<Character>> getMazeArray(){
+        return mazeArray;
+    }
+}
